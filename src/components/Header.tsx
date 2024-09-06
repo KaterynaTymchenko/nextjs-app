e6cff52d6c82ddce02b2497e69558caffbf12237
Kateryna Tymchenko <kateryna.tymchenko@ukr.net>
2024-09-06 20:13:01 +0300

    Added styles for menus

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,28 +6,48 @@ import Logo from '../../assets/logo.png';
 import Image from 'next/image';
 import type { MenuProps } from 'antd';
 import { Menu, Layout } from 'antd';
-import { useState } from 'react';
+import { CSSProperties, useState } from 'react';
 import styles from '@/src/styles/Layout.module.css'; 
+import { SearchOutlined, UserOutlined, HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 
 const { Header } = Layout;
 
 type MenuItem = Required<MenuProps>['items'][number]
 
-const items: MenuItem[] = [
+const menuItems: MenuItem[] = [
   {
-    label: <Link href={Routes.home}>Home</Link>,
+    label: <Link href={Routes.home} >Home</Link>,
     key: 'home',
   },
   {
-    label: <Link href={Routes.shop}>Shop</Link>,
+    label: <Link href={Routes.shop} >Shop</Link>,
     key: 'shop',
   },
   {
-    label: <Link href={Routes.about}>About</Link>,
+    label: <Link href={Routes.about} >About</Link>,
     key: 'about',
   },
   {
-    label: <Link href={Routes.contact}>Contact</Link>,
+    label: <Link href={Routes.contact} >Contact</Link>,
+    key: 'contact'
+  },
+]
+
+const menuIconItems: MenuItem[] = [
+  {
+    icon: <UserOutlined style={{fontSize: 22}}/>,
+    key: 'home',
+  },
+  {
+    icon: <SearchOutlined style={{fontSize: 22}}/>,
+    key: 'about',
+  },
+  {
+    icon: <HeartOutlined style={{fontSize: 22}}/>,
+    key: 'shop',
+  },
+  {
+    icon: <ShoppingCartOutlined style={{fontSize: 22}}/>,
     key: 'contact'
   },
 ]
@@ -47,11 +67,20 @@ const items: MenuItem[] = [
       </Link>
 
       <Menu
+        className={styles.menuItems}
         onClick={onClick}
         selectedKeys={[current]}
         mode="horizontal"
-        items={items}
+        items={menuItems}
       />
+
+      <Menu
+        className={styles.menuIconItems}
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={menuIconItems}
+      />      
     </Header>
   )
 }
